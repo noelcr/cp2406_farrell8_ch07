b3ae7568a832daeb0e4479c6ba4fa621248dd398
jc321443 <noel.chillerroberts@my.jcu.edu.au>
2016-08-29 10:51:34 +1000

    reeee

---
+++ CodeInFigures/BabyNameComparison.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jc321443 on 29/08/16.
+ */
+public class BabyNameComparison {
+    public static void main(String []args){
+        String name1 = "Hugo";
+        String name2 = "Jack";
+        String name3 = "Charlie";
+
+        System.out.println(name1 + " " + name2);
+        System.out.println(name1 + " " + name3);
+        System.out.println(name2 + " " + name1);
+        System.out.println(name2 + " " + name3);
+        System.out.println(name3 + " " + name1);
+        System.out.println(name3 + " " + name2);
+
+
+
+    }
+}
